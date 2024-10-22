b36b732a444ceece3615f62c6b0bd94db83db915
ViniciusRittono <vinicius.rittono@hotmail.com>
2024-10-22 12:45:46 -0300

    Primeiro commit

---
+++ Primitivo/src/Wrappers/Transofrma.java
@@ -0,0 +1,19 @@
+package Wrappers;
+
+public class Transofrma {
+
+public static void main(String[] args) {
+	
+	
+	int numPrimitivo = 10;
+    Integer numWrapper = numPrimitivo;
+    
+  
+    char caracterePrimitivo = 'A';
+    Character caractereWrapper = caracterePrimitivo;
+	
+	}
+}
+
+
+	
